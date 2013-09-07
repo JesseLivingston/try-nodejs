213512afe4ae2fe3141f7228baa6160ebffaacd7
Jesse Livingston <livingston.chen@gmail.com>
2013-09-07 23:51:01 +0800

    add index.js, make server.js as a module

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var http = require("http");
 
-function onRequest(req, res){
-  console.log("Request received.");
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.write("Hello, world!");
-  res.end();
+function start(){
+  function onRequest(req, res){
+    console.log("Request received.");
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Hello, world!");
+    res.end();
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
-http.createServer(onRequest).listen(8888);
 
-console.log("Server has started.");
+exports.start = start;
+
+
+
