b79747ab76c9a192c11d56e38132c2332e315374
Jesse Livingston <livingston.chen@gmail.com>
2013-09-08 09:24:47 +0800

    use correct async

--- request_handlers.js
+++ request_handlers.js
@@ -1,16 +1,19 @@
 var exec = require("child_process").exec;
-function start(){
+function start(res){
 	console.log("Request handler 'start' was called.");
 	var content = "empty";
 	exec("ls -lah", function(error, stdout, stderr){
-		content = stdout;
-	})
-	return content;
+		res.writeHead(200, {"Content-Type": "text/plain"});
+		res.write(stdout);
+		res.end();
+	});
 }
 
-function upload(){
+function upload(res){
 	console.log("Request handler 'upload' was called.");
-	return "Hello, upload";
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.write("Hello, upload");
+	res.end();
 }
 
 exports.start = start;
