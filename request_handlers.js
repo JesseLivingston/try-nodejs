e196b099cffc14af81ec9282727bc91d6684d28d
Jesse Livingston <livingston.chen@gmail.com>
2013-09-08 10:16:39 +0800

    post a text

--- request_handlers.js
+++ request_handlers.js
@@ -1,18 +1,29 @@
 var exec = require("child_process").exec;
-function start(res){
+var querystring = require("querystring");
+function start(res, postData){
 	console.log("Request handler 'start' was called.");
-	var content = "empty";
-	exec("ls -lah", function(error, stdout, stderr){
-		res.writeHead(200, {"Content-Type": "text/plain"});
-		res.write(stdout);
-		res.end();
-	});
+	var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(body);
+    res.end();
 }
 
-function upload(res){
+function upload(res, postData){
 	console.log("Request handler 'upload' was called.");
 	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.write("Hello, upload");
+	res.write("You've send: " + querystring.parse(postData).text);
 	res.end();
 }
 
